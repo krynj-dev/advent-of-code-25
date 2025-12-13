4df91f7b4ab1dbe9cb36f72740a90b67c7afe114
Hayden Baker <hbaker@outlook.com.au>
2025-12-13 14:43:15 +1100

    Rust: day 3 part 2
    Refactored part one as well

--- rust/src/day3.rs
+++ rust/src/day3.rs
@@ -37,29 +37,49 @@ fn parse_input(input: String) -> Vec<String> {
     return parsed;
 }
 
+fn max_char(line: &String, lwr: usize, upr: usize) -> usize {
+    let mut loc = lwr;
+    for i in lwr..upr {
+        if line.chars().nth(i).unwrap() > line.chars().nth(loc).unwrap() {
+            loc = i;
+        }
+    }
+    return loc;
+}
+
+fn get_num(line: &String, digits: usize) -> u128 {
+    let mut n = 0u128;
+    let mut lwr = 0usize;
+    let mut upr = line.len() - digits + 1;
+    for i in 0..digits {
+        let loc = max_char(line, lwr, upr);
+        n += line
+            .chars()
+            .nth(loc)
+            .expect("char loc")
+            .to_digit(10)
+            .expect("digit conversion") as u128
+            * 10u128.pow((digits - i - 1) as u32);
+        lwr = loc + 1;
+        upr += 1;
+    }
+    return n;
+}
+
 fn part_one(parsed: &Vec<String>) -> u128 {
     let mut count: u128 = 0;
     for line in parsed {
-        let mut i = 0;
-        let mut j = 1;
-        for x in 1..line.len() {
-            let c = line.chars().nth(x).unwrap();
-            if line.chars().nth(i).unwrap() < c && x < line.len() - 1 {
-                i = x;
-                j = i + 1;
-            } else if line.chars().nth(j).unwrap() < c {
-                j = x;
-            }
-        }
-        let mut num = String::new();
-        num.push(line.chars().nth(i).unwrap());
-        num.push(line.chars().nth(j).unwrap());
-        count += num.parse::<u128>().unwrap();
+        let x = get_num(line, 2usize);
+        count += x;
     }
     return count;
 }
 
 fn part_two(parsed: &Vec<String>) -> u128 {
     let mut count: u128 = 0;
+    for line in parsed {
+        let x = get_num(line, 12usize);
+        count += x;
+    }
     return count;
 }
